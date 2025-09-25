06b9b35b16cda4c6a3a8a85c57bf5bc392819d0e
Enrique <taguniversalsoftware@gmail.com>
2025-09-25 16:12:39 -0500

    fix styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/forms'), require('daisyui'),
-
+    require('@tailwindcss/typography'), 
+    require('daisyui'),
   ],
 };
